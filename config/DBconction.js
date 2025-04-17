dbcd6d24e8ea7c72451bea3d9614b84bb752ad23
YoussefAhmed100 <youssefahmed16110@gmail.com>
2025-04-18 00:32:30 +0200

    apdate data connection function

--- config/DBconction.js
+++ config/DBconction.js
@@ -1,20 +1,16 @@
 const mongoose = require("mongoose");
 require('colors');
 
-const databaseConction =(req ,res )=>{
-    
-mongoose
-.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+const databaseConction = () => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("✅ MongoDB connection successful".green.inverse);
+    })
+    .catch((err) => {
+      console.error(`❌ Database connection error: ${err.message}`.red.bold);
+      process.exit(1);
+    });
+}; 
 
-.then((res) => {
-  console.log("mongodb connection successfully".red.inverse);
-})
-// .catch((err) => {
-//   console.log(`Database error${err}`);
-//   process.exit(1);
-// });
-}
-module.exports = databaseConction; 
+module.exports = databaseConction;
